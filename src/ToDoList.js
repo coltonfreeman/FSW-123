fa2e3c1ac17f159b671afa8eb326ca2f42e198ef
Colton Freeman <91649148+coltonfreeman@users.noreply.github.com>
2022-03-28 00:09:05 -0500

    added assignment

---
+++ src/ToDoList.js
@@ -0,0 +1,17 @@
+import './App.css'
+
+function CreateList() {
+    return (
+        <>
+            <h1>Colton's To-Do List</h1>
+            <ul>
+                <li><input type="checkbox"/>Work out</li>
+                <li><input type="checkbox"/>Do school work and assignments</li>
+                <li><input type="checkbox"/>Prep dinner</li>
+                <li><input type="checkbox"/>Watch new movie</li>
+            </ul>
+        </>
+    );
+}
+
+export default CreateList;
