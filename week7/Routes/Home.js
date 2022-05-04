d61d07881d5e349ef0a5e427673b500a8a316714
Colton Freeman <91649148+coltonfreeman@users.noreply.github.com>
2022-05-03 17:30:18 -0700

    update week7

---
+++ week7/Routes/Home.js
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+        <div className="homePageDiv">
+            <h1>FSW-123: Capstone Project</h1>
+            <img id="homePageImg" src="https://laffgaff.com/wp-content/uploads/2021/08/programmer-jokes-puns-featured.jpg" alt="by C.Freeman"></img>
+            <h2>By: Colton Freeman &#169;2022</h2>
+        </div>
+    );
+}
+
+export default Home;
---
+++ week7/Routes/NoMatch.js
@@ -0,0 +1,7 @@
+function NoMatch() {
+    return(
+        <h1>Sorry No Results Found</h1>
+    )
+}
+
+export default NoMatch;
